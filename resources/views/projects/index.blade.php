fee4dc594cfb9d23a9aa9e7560f035ed0f346dc0
Juan Carlos Orrego <jcorrego@gmail.com>
2015-05-05 11:47:21 -0500

    Projects: Summary display updates

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,8 +6,12 @@
 @stop
 
 @section('content')
-    <h1>@lang('projects/title.projects')</h1>
-    @foreach($projects as $project)
-        @include('projects.summary')
-    @endforeach
+    <h1>@lang('projects/title.recommended')</h1>
+    <div class="row projects">
+        @foreach($projects as $project)
+            <div class="project col-md-3">
+                @include('projects.summary')
+            </div>
+        @endforeach
+    </div>
 @stop
--- resources/views/projects/summary.blade.php
+++ resources/views/projects/summary.blade.php
@@ -1,4 +1,15 @@
-<div>
-    <h2><a href="{{ action('ProjectsController@show',[$project->id]) }}">{{ $project->name }}</a></h2>
-    <div class="body">{{ $project->description}}</div>
+<div class="content">
+    <div class="image"><img class="img-responsive" src="{{ imagestyle($project->image,'fit330x297') }}"
+                            alt="{{ $project->name }}"/></div>
+    <div class="fields">
+        <div class="title"><h4><a href="{{ action('ProjectsController@show',[$project->id]) }}">{{ $project->name }}</a>
+            </h4></div>
+        <div class="body">{{ $project->description}}</div>
+        <div class="more-link"><a href="{{ action('ProjectsController@show',[$project->id]) }}">Read more +</a></div>
+        <div class="links">
+            <a class="button btn " href="">Like</a>
+            <a class="button btn " href="">Share</a>
+            <a class="button btn " href="">Participate</a>
+        </div>
+    </div>
 </div>
