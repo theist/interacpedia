3adb428c46e8f4b84791da8d8e9dc97c53729d21
Juan Carlos Orrego <jcorrego@gmail.com>
2015-05-08 03:09:45 -0500

    Removed image resize restriction in 100x100 filter

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -9,8 +9,10 @@ function imagestyle($image, $style="small"){
         $img = Image::make($image)->encode('jpg')->save( public_path(). $file );
         $user->avatar = $file;
         $user->save;
-        return $file;
-    } else if(strstr($image,"images/")){
+        $image = $file;
+    }
+
+    if(strstr($image,"images/")){
         return str_replace("images/","images/cache/".$style."/",$image);
     } else{
         return "/images/cache/".$style."/".$image;
