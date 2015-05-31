8fd280c1e3aafd9a668dcfe24264d7a5ad1870c2
Juan Carlos Orrego <jcorrego@gmail.com>
2015-05-31 12:50:23 -0500

    Initial seeders texts updates

--- database/seeds/ChallengeTableSeeder.php
+++ database/seeds/ChallengeTableSeeder.php
@@ -11,18 +11,18 @@ class ChallengeTableSeeder extends Seeder {
         Challenge::create( [
             'name'        => 'StreetAdventure',
             'description' => 'Como aprender a recorrer ciudades de una forma divertida, utilizando una metodología preventiva que permita aprender que hacer en caso de un desastre.',
-            'image'       => '/images/challenges/carpooling.jpg',
+            'image'       => '/images/challenges/streetadventure.png',
             'user_id'     => 3
         ] );
         Challenge::create( [
             'name'        => 'Crear aplicación de "Car Pooling" para universidades',
             'description' => 'Ayúdanos a mejorar la congestión de vehículos en nuestras universidades y en otros lugares.',
-            'image'       => '/images/challenges/streetaventure.png',
+            'image'       => '/images/challenges/carpooling.jpg',
             'user_id'     => 1
         ] );
         for($i=2; $i<=4;$i++){
             Challenge::create( [
-                'name'        => 'Reto número'.$i,
+                'name'        => 'Reto número '.$i,
                 'description' => 'Texto descriptivo creado de manera aleatoria para un reto aleatorio creado con el número '.$i.'.',
                 'image'       => '/images/challenges/ch0'.$i.'.jpg',
                 'user_id'     => 1
