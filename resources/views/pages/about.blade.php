f2a6f727a9a27b47c3927a1e4a66f77e7a84d612
Juan Carlos Orrego <jcorrego@gmail.com>
2015-06-08 13:08:44 -0500

    About Us changes

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -41,11 +41,52 @@
                 </div>
             </div>
         </div>
+        <div class="row text-center phrase">“Algunos de los mayores proyectos en la historia de la humanidad necesitaron más de 100.000 personas"</div>
+        <div class="row text-center"><img src="/images/backgrounds/neurons.png" alt="Interacpedia"/></div>
+        <div class="row text-center"><h4>¿Que podríamos lograr si unimos 1.4 billones de estudiantes en el mundo?</h4></div>
+
+        <div class="row text-center"><img src="/images/backgrounds/our-team.jpg" alt="Interacpedia"/></div>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="row our-team">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-3">
+                        <div class="row">
+                            <div class="col-md-12 text-center"><img src="/images/backgrounds/aboutus-santi.png" alt="Santiago Lalinde"/></div>
+                            <div class="col-md-12 name">Santiago Lalinde</div>
+                            <div class="col-md-12 summary">Administrador de Negocios especializado en Finanzas y Negocios por Internet con mas de 12 años de Experiencia en Marketing, Investigación de Mercados, Sistemas de Información, Ventas y Alta Gerencia. Creador de software y juegos (Copyright) con reconocimientos académicos, creatividad y diseño de procesos.</div>
+                            <div class="col-md-12 position">CEO</div>
+                        </div>
+                    </div>
+                    <div class="col-md-1"></div>
+                    <div class="col-md-3">
+                        <div class="row">
+                            <div class="col-md-12 text-center"><img src="/images/backgrounds/aboutus-juank.png" alt="Juan Carlos Orrego"/></div>
+                            <div class="col-md-12 name">Juan Carlos Orrego</div>
+                            <div class="col-md-12 summary">Ingeniero de Sistemas con más de 15 años de experiencia en creación, ejecución y gestión de mas de 200 proyectos digitales en América Latina.  Asesor del sector público y privado, en materia tecnológica y estrategia digital.</div>
+                            <div class="col-md-12 position">CTO</div>
+                        </div>
+                    </div>
+                    <div class="col-md-1"></div>
+                    <div class="col-md-3">
+                        <div class="row">
+                            <div class="col-md-12 text-center"><img src="/images/backgrounds/aboutus-fefe.png" alt="Maria Fernanda Escobar"/></div>
+                            <div class="col-md-12 name">Maria Fernanda Escobar</div>
+                            <div class="col-md-12 summary">Diseñadora gráfica, especializada en empaques y desarrollo de marca, con más de 8 años de experiencia en diseño de identidad corporativa, comunicación, publicidad, arte y manejo editorial a nivel local e internacional. Ganadora de beca por mejor Icfes.</div>
+                            <div class="col-md-12 position">CDO</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="row text-center"><img src="/images/backgrounds/neurons-2.png" alt="Interacpedia"/></div>
+        <div class="row text-center phrase">Asesores Expertos en relaciones internacionales, operaciones, finanzas, estrategia, comunicaciones, emprendimiento y educación.</div>
+        <div class="row text-center"><img src="/images/backgrounds/aboutus-creativity.jpg" alt="Ideaminds"/></div>
         <div class="row text-center">
-            <h4>“Algunos de los mayores proyectos en la historia de la humanidad necesitaron más de 100.000 personas"</h4>
+            Creado por: <br/>
+            <img src="/images/backgrounds/aboutus-ideaminds.png" alt="Ideaminds"/>
+            &copy;{{ date("Y") }} IDEAMINDS. Todos los derechos reservados. <br/>
+            www.ideaminds.net
         </div>
-        <div class="text-center">
-            <img src="/images/backgrounds/neurons.png" alt="Interacpedia"/>
-        </div>
-    </div>
 @stop
