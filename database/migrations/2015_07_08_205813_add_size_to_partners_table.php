5fc5c39bf716a5cc19ac97ea888f5acdf710b122
Juan Carlos Orrego <jcorrego@gmail.com>
2015-07-08 15:59:23 -0500

    Add size field to partners table

---
+++ database/migrations/2015_07_08_205813_add_size_to_partners_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSizeToPartnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('partners', function (Blueprint $table) {
+            $table->string('size')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('partners', function (Blueprint $table) {
+            $table->dropColumn('size');
+        });
+    }
+}
