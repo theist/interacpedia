f3a012b05be5459be90e0d870183bee8deebffa3
Juan Carlos Orrego <jcorrego@gmail.com>
2015-07-12 23:40:16 -0500

    Footer updates

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,50 +1,34 @@
 <div class="container">
     <div class="row">
-        <div class="col-md-2">
+        <div class="col-md-2 text-center">
             <h4>SOBRE NOSOTROS</h4>
             <ul>
-                <li>Qué es Interacpedia?</li>
-                <li>Quiénes somos?</li>
-                <li>Prensa</li>
-                <li>Estadísticas</li>
-                <li>Proyectos aliados</li>
+                <li><a href="/about-us">¿Qué es Interacpedia?</a></li>
+
             </ul>
         </div>
-        <div class="col-md-2">
+        <div class="col-md-2 text-center">
             <h4>AYUDA</h4>
             <ul>
-                <li>FAQ</li>
-                <li>Nuestras Reglas</li>
+                <li><a href="/how-it-works">¿Cómo funciona?</a></li>
                 <li><a href="/terms-of-use">Términos de Uso</a></li>
-                <li><a href="/privacy-policy">Política de Privacidad</a></li>
+                <li><a href="/#">Política de Privacidad</a></li>
             </ul>
         </div>
-        <div class="col-md-6">
-            <h4>DESCUBRA</h4>
-
-            <div class="row">
-                <div class="col-md-4">
-                    <ul>
-                        <li>Arte</li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <ul>
-                        <li>Apps</li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <ul>
-                        <li>Mercadeo</li>
-                    </ul>
-                </div>
-            </div>
+        <div class="col-md-6 text-center">
+            <br>
+            <br>
+            <a href="https://www.facebook.com/interacpedia" target="_blank"><img src="/images/icons/social-networks/facebook.png" alt="Facebook"></a>
+            <a href="https://twitter.com/interacpedia" target="_blank"><img src="/images/icons/social-networks/twitter.png" alt="Twitter"></a>
+            <a href="https://instagram.com/interacpedia" target="_blank"><img src="/images/icons/social-networks/instagram.png" alt="Instagram"></a>
+            <a href="https://www.pinterest.com/interacpedia/" target="_blank"><img src="/images/icons/social-networks/pinterest.png" alt="Pinterest"></a>
+            <a href="#" target="_blank"><img src="/images/icons/social-networks/linkedin.png" alt="LinkedIn"></a>
+            <a href="https://blog.interacpedia.com" target="_blank"><img src="/images/icons/social-networks/blogger.png" alt="Blogger"></a>
+            <a href="#" target="_blank"><img src="/images/icons/social-networks/youtube.png" alt="YouTube"></a>
         </div>
-        <div class="col-md-2">
+        <div class="col-md-2 text-center">
             <h4>CONTACTO</h4>
-            <ul>
-                <li>Noticias</li>
-            </ul>
+            <div><img src="/images/logos/ideaminds-gray.png" alt="Ideaminds"></div>
         </div>
 
     </div>
