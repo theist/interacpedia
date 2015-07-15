55454601e344aeaecfeb101e7894b8c7cc77867e
Juan Carlos Orrego <jcorrego@gmail.com>
2015-07-14 23:56:16 -0500

    Blog posts preview and mentors image

--- resources/views/blogs/post.blade.php
+++ resources/views/blogs/post.blade.php
@@ -1,7 +1,7 @@
 <div class="post row">
     <div class="col-md-12">
         <h5>{{ $post->title }}</h5>
-        {!! $post->content !!}
+        {!! str_limit( strip_tags($post->content,"<img>"),400) !!}
         <hr/>
     </div>
 </div>
