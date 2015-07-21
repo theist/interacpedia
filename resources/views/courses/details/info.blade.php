ea8f599e1f0ca99efcd9af57e350d37da54cff8b
Juan Carlos Orrego <jcorrego@gmail.com>
2015-07-21 00:48:52 -0500

    Facebook block

--- resources/views/courses/details/info.blade.php
+++ resources/views/courses/details/info.blade.php
@@ -38,5 +38,14 @@
         <div class="content">
             <img class="img-responsive" src="/images/demo/facebook-block.jpg" alt="Facebook">
         </div>
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.4&appId=1579172622347450";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));</script>
+            <div class="fb-page" data-href="https://www.facebook.com/interacpedia" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/interacpedia"><a href="https://www.facebook.com/interacpedia">Interacpedia</a></blockquote></div></div>
     </div>
 </div>
