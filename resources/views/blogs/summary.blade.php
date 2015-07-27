9ceabf9022753eda4adb15094fa9c8c21eac07cb
Juan Carlos Orrego <jcorrego@gmail.com>
2015-07-27 14:07:59 -0500

    Course Blog display and links

--- resources/views/blogs/summary.blade.php
+++ resources/views/blogs/summary.blade.php
@@ -4,6 +4,7 @@
         <a target="_blank" href="{{ $post->url }}">{{ $post->url }}</a>
         <div class="author"><strong>@lang('general/labels.by'):</strong> {{ $post->author->displayName }}</div>
         <div class="date"><strong>@lang('general/labels.published'):</strong> {{ $post->published }}</div>
+        <div class="comments"><strong>@lang('general/labels.comments'):</strong> {{ $post->replies->totalItems }}</div>
         <hr/>
     </div>
 </div>
