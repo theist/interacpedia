15b50af40346cc5a8d16036451dd7799c62a1b71
Juan Carlos Orrego <jcorrego@gmail.com>
2015-08-25 00:48:53 -0500

    Teams on Courses

--- resources/views/courses/details/teams.blade.php
+++ resources/views/courses/details/teams.blade.php
@@ -5,32 +5,21 @@
             <small>A continuación se muestran los equipos de trabajo de los estudiantes de esta clase.</small>
             <hr/>
             <div class="row">
-            @foreach($teams as $team)
-                <div class="col-md-6 team">
-                    <h4>{{ trans_choice('general/labels.teams', 1) }} # {{ $team->id }}<br>
-                        <small>{{ $team->challenge->name }}</small></h4>
-                    <div class="row content">
-                    @foreach($team->users as $st)
-                        <div class="col-md-6 text-center student">
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <a href="/user/{{ $st->id }}">
-                                        <img class="img-circle" height="50" src="{{ imagestyle($st->avatar,'fit100x100') }}"
-                                             alt="{{ $st->name }}"/>
-                                    </a>
+                @foreach($teams as $team)
+                    <div class="col-md-12 team">
+                        <h4>{{ trans_choice('general/labels.teams', 1) }} # {{ $team->id }}<br>
+                            <small>{{ $team->challenge->name }}</small>
+                        </h4>
+                        <div class="row content users">
+                            @foreach($team->users as $st)
+                                <div class="user summary col-md-6 {{ $st->agree?'agree':'not-agree' }}">
+                                    @include('user.summary',['user'=>$st])
                                 </div>
-                                <div class="name text-left col-md-9">
-                                    <a href="/user/{{ $st->id }}">{{ $st->name }}</a>
-                                </div>
-                            </div>
+                            @endforeach
                         </div>
-                    @endforeach
+                        <hr/>
                     </div>
-{{--                    @include('teams.summary',['post'=>$post,'model'=>$course])--}}
-                    <hr/>
-                </div>
-
-            @endforeach
+                @endforeach
             </div>
         </div>
     </div>
