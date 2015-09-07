5dcc2b4326b8fa802817b795d0743b05619c7dc9
Juan Carlos Orrego <jcorrego@gmail.com>
2015-09-07 13:16:56 -0500

    Better message email response text

--- resources/views/emails/newmessage.blade.php
+++ resources/views/emails/newmessage.blade.php
@@ -9,10 +9,15 @@
         <div style="color:#999;font-size:14px;">
             <h5>{{ $model['title'] }}</h5>
             <p>{{ $model['content'] }}</p>
+            @if($model['message_id'] && $model['message_id']>0)
+                <a style="padding: 10px 20px;background-color: #5C478E;border-radius: 5px;color: white;text-decoration: none;" href="{{ url("messages/".$model['message_id']) }}">Responder</a>.
+            @else
+                <a style="padding: 10px 20px;background-color: #5C478E;border-radius: 5px;color: white;text-decoration: none;" href="{{ url("messages/".$model['id']) }}">Responder</a>.
+            @endif
         </div>
         <br/>
         <hr/>
-        <div style="text-align: center;font-size:10px;">
+        <div style="text-align: center;font-size:14px;">
             @if($model['message_id'] && $model['message_id']>0)
                 Para ver o responder este mensaje, haz click <a href="{{ url("messages/".$model['message_id']) }}">aquí</a>.
             @else
