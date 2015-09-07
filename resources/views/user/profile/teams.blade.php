337df57b3d7f7abda15f4cd132d9b9639f7f194c
Juan Carlos Orrego <jcorrego@gmail.com>
2015-09-07 17:28:51 -0500

    teams title update

--- resources/views/user/profile/teams.blade.php
+++ resources/views/user/profile/teams.blade.php
@@ -1,6 +1,6 @@
 <div class="subtitle">
-    <img src="/images/icons/40x40/process.png" alt="@lang('general/labels.teams')"/>
-    @lang('general/labels.teams')
+    <img src="/images/icons/40x40/process.png" alt="{{ trans_choice('general/labels.teams',2) }}"/>
+    {{ trans_choice('general/labels.teams',2) }}
 </div>
 <div class="row teams">
     @foreach($user->teams as $team)
