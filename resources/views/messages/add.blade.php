beab6905125b351db48e94df798488d1f8e0b080
Juan Carlos Orrego <jcorrego@gmail.com>
2015-09-08 20:57:50 -0500

    Messages Add correction

--- resources/views/messages/add.blade.php
+++ resources/views/messages/add.blade.php
@@ -48,7 +48,7 @@
     <link href="/css/selectize.default.css" rel="stylesheet">
 @stop
 @section('scripts')
-    <script src="/js/selectize.js"></script>
+    @parent
     <script>
         $('#addmessage-form{{ $suffix }}').on('submit', function (e) {
             e.preventDefault();
@@ -74,76 +74,6 @@
                     }
             );
             return false;
-        });
-
-        $(function() {
-            var REGEX_EMAIL = '([a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@' +
-                    '(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)';
-
-            $(".selectize").selectize({
-                persist: false,
-                valueField: 'id',
-                labelField: 'name',
-                searchField: ['name', 'email'],
-                options: [
-                        @if(isset($users))
-                        @foreach($users as $us)
-                        {id:{{ $us->id }}, email: '{{ $us->email }}', name: '{{ $us->name }}'},
-                        @endforeach
-                        @endif
-                ],
-                render: {
-                    item: function(item, escape) {
-                        return '<div>' +
-                                (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '')  +
-                                '</div>';
-                    },
-                    option: function(item, escape) {
-                        var label = item.name || item.email;
-                        var caption = item.name ? item.email : null;
-                        return '<div>' +
-                                '<span class="name">' + escape(label) + '</span>' +
-                                (caption ? '<span class="caption">' + escape(caption) + '</span>' : '') +
-                                '</div>';
-                    }
-                },
-                createFilter: function(input) {
-                    var match, regex;
-
-                    // email@address.com
-                    regex = new RegExp('^' + REGEX_EMAIL + '$', 'i');
-                    match = input.match(regex);
-                    if (match) return !this.options.hasOwnProperty(match[0]);
-
-                    // name <email@address.com>
-                    regex = new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i');
-                    match = input.match(regex);
-                    if (match) return !this.options.hasOwnProperty(match[2]);
-
-                    return false;
-                },
-                create: function(input) {
-                    if ((new RegExp('^' + REGEX_EMAIL + '$', 'i')).test(input)) {
-                        return {email: input};
-                    }
-                    var match = input.match(new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i'));
-                    if (match) {
-                        return {
-                            email : match[2],
-                            name  : $.trim(match[1])
-                        };
-                    }
-                    alert('Invalid email address.');
-                    return false;
-                }
-            });
-            $('.maxlength').maxlength({
-                alwaysShow: true,
-                warningClass: "label label-success",
-                limitReachedClass: "label label-danger",
-                appendToParent: true,
-                placement: "bottom-right"
-            });
         });
     </script>
 @stop
