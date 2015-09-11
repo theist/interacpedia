21fa4886e1db0d7bd0c73c6080dfe3230f523f46
Juan Carlos Orrego <jcorrego@gmail.com>
2015-09-11 06:54:38 -0500

    Events display updates

--- app/Interacpedia/Event.php
+++ app/Interacpedia/Event.php
@@ -2,6 +2,7 @@
 
 namespace App\Interacpedia;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
