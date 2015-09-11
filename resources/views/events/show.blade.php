c0e822b8ddaa2d5d15feebd666bf1a53eef28a63
Juan Carlos Orrego <jcorrego@gmail.com>
2015-09-11 07:10:04 -0500

    Events gallery thumbnail display

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -35,3 +35,16 @@
         @include('events.details.'.$option,['event'=>$event])
     </div>
 @stop
+@section('footer')
+    @parent
+    <script>
+        $(document).delegate('*[data-toggle="lightbox"]', 'click', function (event) {
+            event.preventDefault();
+            $(this).ekkoLightbox({
+                always_show_close: true,
+                left_arrow_class: 'fa fa-chevron-left glyphicon-chevron-left',
+                right_arrow_class: 'fa fa-chevron-right glyphicon-chevron-right'
+            });
+        });
+    </script>
+@stop
