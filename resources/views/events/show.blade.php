3233dcd72764b5da6e4eb132089622c4ff1eeade
Juan Carlos Orrego <jcorrego@gmail.com>
2015-09-18 10:58:20 -0500

    Events gallery upgrades

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -40,11 +40,7 @@
     <script>
         $(document).delegate('*[data-toggle="lightbox"]', 'click', function (event) {
             event.preventDefault();
-            $(this).ekkoLightbox({
-                always_show_close: true,
-                left_arrow_class: 'fa fa-chevron-left glyphicon-chevron-left',
-                right_arrow_class: 'fa fa-chevron-right glyphicon-chevron-right'
-            });
+            $(this).ekkoLightbox();
         });
     </script>
 @stop
