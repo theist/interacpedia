cfd54753dd7f83511bca3bcc9fbabea13711d357
Juan Carlos Orrego <jcorrego@gmail.com>
2015-09-29 10:38:52 -0500

    About Us- Spanish update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -84,6 +84,44 @@
         </div>
         <div class="row text-center"><img class="img-responsive" src="/images/backgrounds/neurons-2.png" alt="Interacpedia"/></div>
         <div class="row text-center phrase">Asesores Expertos en relaciones internacionales, operaciones, finanzas, estrategia, comunicaciones, emprendimiento y educación.</div>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="row our-team">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-5">
+                        <div class="row">
+                            <div class="col-md-12 text-center"><img src="/images/backgrounds/aboutus-lucas.png" alt="Santiago Lalinde"/></div>
+                            <div class="col-md-12 name">Lucas Osorio</div>
+                            <div class="col-md-12 summary"><strong>Exxon Mobil Qatar</strong><br>
+                                Ingeniero Electrónico - Maestría Robótica Georgia Tech. <br>
+                                +10 años en planeación estratégica, evaluación, proyectos y pxn en la industria petrolera. Liderado proyectos y operaciones en EEUU, España, Italia, y Qatar.
+                                <br>
+                                Programación, automatización y robótica. <br>
+                                Múltiples becas y premios, Lider de proyectos  +U$100MM. Identificado por la gerencia como futuro líder Internacional.
+                            </div>
+                            <div class="col-md-12 position">CRO</div>
+                        </div>
+                    </div>
+                    <div class="col-md-1"></div>
+                    <div class="col-md-5">
+                        <div class="row">
+                            <div class="col-md-12 text-center"><img src="/images/backgrounds/aboutus-caro.png" alt="Juan Carlos Orrego"/></div>
+                            <div class="col-md-12 name">Carolina Tejada</div>
+                            <div class="col-md-12 summary">Atlas Corps Fellow at <strong>Mozilla</strong> Foundation
+                                <br>
+                                Comunicadora con Posgrado en Sociología y Ciencia Política <br>
+                                New York - USA <br>
+                                10 años de experiencia en comunicación organizacional, formación, planeación de eventos, diseño y producción de medios impresos y audiovisuales. TCC, Fenalco, Organzación VID y Leonisa.
+                                <br>
+                                Directora Administrativa de Causa & Efecto: Desarrollo de competencias y cultura competitiva para desarrollar al máximo el potencial de las empresas del país.
+                            </div>
+                            <div class="col-md-12 position"></div>
+                        </div>
+                    </div>
+                    <div class="col-md-1"></div>
+                </div>
+            </div>
+        </div>
         <div class="row text-center"><img class="img-responsive" src="/images/backgrounds/aboutus-creativity.jpg" alt="Ideaminds"/></div>
         <div class="row text-center ideaminds">
             <div class="created">Creado por:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
